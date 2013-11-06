53710782f03863cd79e0de92164424cf60c55011
sergiu <sergiu@debian>
2013-11-06 14:54:59 +0200

    laboratory56

--- kmain.c
+++ kmain.c
@@ -3,33 +3,106 @@
 #include "lib/video.c"
 #include "lib/longjmp.c"
 
-int strcmp(char *s1,char *s2)
+
+#define TASK_STACK_SIZE 512
+
+typedef enum 
 {
-int i = 0,c = 0;
-while (1)
-{ 
-  if (i == 5) break;
-  if (s1[i] == s2[i]) c++;
-  i++;
+  TS_RUNNABLE = 1, TS_BLOCKED = 2, TS_ZOMBIE = 3
+} task_status_t;
+
+
+typedef struct 
+{
+    jmp_buf state;
+    task_status_t status;
+    unsigned char stack[TASK_STACK_SIZE];
+} task_t;
+
+
+task_t tasks[2];
+int task_nr = 0;
+current_task_nr = 0;
+
+
+void sched_add_task(void * func_ptr) 
+{
+
+    task_t * task = & tasks[task_nr];
+    task_nr++;
+
+    int i;
+    i = setjmp(task->state);
+    if (!i) 
+    {
+        unsigned * initial_stack_ptr;
+        initial_stack_ptr = (unsigned) task->stack + TASK_STACK_SIZE;
+
+        task->state[0].esp = initial_stack_ptr;
+        task->state[0].eip = func_ptr;
+        task->state[0].eflags = 0x200; 
+     }
 }
-if (i == c) return 1;
-return 0;
+
+
+void sched_run() 
+{
+    current_task_nr = 0;
+    longjmp(tasks[0].state, 1);
+}
+
+
+void yield() 
+{
+    if (!setjmp(tasks[current_task_nr].state)) 
+    {
+        if (current_task_nr == 0) 
+        {
+            current_task_nr = 1;
+        }
+         else 
+         {
+            current_task_nr = 0;
+        }
+        longjmp(tasks[current_task_nr].state, 1);
+    }
+}
+
+char * vidmem = (char *) 0xb8000;
+
+void wait() 
+{
+  long int a;
+  for ( a = 1 ; a <= 34343000 ; a++ ) ;
+}
+
+void task0 (void) 
+{
+  while (1) 
+  {
+    yield();
+    vidmem[0] = vidmem[0] == 'B' ? 'A' : 'B';
+    wait();
+  }
+}
+
+void task1 (void) 
+{
+  while (1) 
+  {
+    yield();
+    vidmem[2] = vidmem[2] == 'B' ? 'A' : 'B';
+    wait();
+  }
 }
 
 int main( void )
 {
- char a[1000];
- puts ("welcome to OS \n");
- for (;;)
-   {
-    puts("\n");
-    puts("$:");
-    gets(a);
-    clrscr();
-    puts(a);
-    puts("\n");
-    if (strcmp(a, "hello")==1){clrscr(); cmd_hello(); }
-    }
-
+	vidmem[0]= 'C';
+  vidmem[1] = 0x7;
+  vidmem[3] = 0x7;
+  sched_add_task(task0);
+  sched_add_task(task1);
+  sched_run();
 }
 
